12c16c78d208c5cde7e29dcc52e610ed9d502c32
Cha <chalaalemayehu1824@gmail.com>
2025-12-14 16:14:47 +0300

    very_first commit

---
+++ android/app/src/main/kotlin/com/example/face_recogintion_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.face_recogintion_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'HomeScreen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(home: HomeScreen(), debugShowCheckedModeBanner: false),
+  );
+}
